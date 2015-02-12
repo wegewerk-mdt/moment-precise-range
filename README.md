20d56fbb5b25d69339cfb3456c50d6b593a56fb4
mdt <mdt@wegewerk.com>
2015-02-12 17:18:06 +0000

    Added i18n support

---
+++ README.md
@@ -0,0 +1,31 @@
+# Moment Date Range Plugin
+
+This is a plugin for the momentjs Javascript Library.
+It adds a function to display the difference between 2 dates precisely.
+
+It is a fork from http://codebox.org.uk/pages/moment-date-range-plugin
+
+I added locale Support and short notation support
+
+## Example
+
+<code language="javascript">
+    a = moment("2013-02-08 09:30:26");
+    b = moment("2013-02-08 12:35:00");
+
+    # default behaviour
+    moment.locale('en');
+    moment.preciseDiff(a,b);
+    #"3 hours 4 minutes a few seconds"
+
+    # set other locale
+    moment.locale('de');
+    moment.preciseDiff(a,b);
+    #"3 Stunden 4 Minuten ein paar Sekunden"
+
+    # short format (no i18n support)
+    moment.preciseDiff(a,b,{hour:true,minute:true,short:true})
+    # "3h 4min"
+</code>
+
+## Installation
--- readable-range.js
+++ readable-range.js
@@ -1,91 +1,104 @@
 (function(moment) {
-    var STRINGS = {
-        nodiff: '',
-        year: 'year',
-        years: 'years',
-        month: 'month',
-        months: 'months',
-        day: 'day',
-        days: 'days',
-        hour: 'hour',
-        hours: 'hours',
-        minute: 'minute',
-        minutes: 'minutes',
-        second: 'second',
-        seconds: 'seconds',
-        delimiter: ' '
+    var shortUnits = {
+        year:'a',
+        month: 'mon',
+        day: 'd',
+        hour:'h',
+        minute:'min',
+        second:'s'
     };
-    moment.fn.preciseDiff = function(d2) {
-        return moment.preciseDiff(this, d2);
+
+    moment.fn.preciseDiff = function(d2, opts) {
+      return moment.preciseDiff(this, d2, opts);
     };
-    moment.preciseDiff = function(d1, d2) {
-        var m1 = moment(d1), m2 = moment(d2);
-        if (m1.isSame(m2)) {
-            return STRINGS.nodiff;
-        }
-        if (m1.isAfter(m2)) {
-            var tmp = m1;
-            m1 = m2;
-            m2 = tmp;
-        }
+    moment.preciseDiff = function(d1, d2, opts) {
+      var m1 = moment(d1), m2 = moment(d2);
+      opts = opts || {
+        year: true,
+        month: true,
+        day: true,
+        hour: true,
+        minute: true,
+        second: true,
+        short:false
+      }
 
-        var yDiff = m2.year() - m1.year();
-        var mDiff = m2.month() - m1.month();
-        var dDiff = m2.date() - m1.date();
-        var hourDiff = m2.hour() - m1.hour();
-        var minDiff = m2.minute() - m1.minute();
-        var secDiff = m2.second() - m1.second();
+      if (m1.isSame(m2)) {
+        return '';
+      }
+      if (m1.isAfter(m2)) {
+        var tmp = m1;
+        m1 = m2;
+        m2 = tmp;
+      }
 
-        if (secDiff < 0) {
-            secDiff = 60 + secDiff;
-            minDiff--;
-        }
-        if (minDiff < 0) {
-            minDiff = 60 + minDiff;
-            hourDiff--;
-        }
-        if (hourDiff < 0) {
-            hourDiff = 24 + hourDiff;
-            dDiff--;
-        }
-        if (dDiff < 0) {
-            var daysInLastFullMonth = moment(m2.year() + '-' + (m2.month() + 1), "YYYY-MM").subtract('months', 1).daysInMonth();
-            if (daysInLastFullMonth < m1.date()) { // 31/01 -> 2/03
-                dDiff = daysInLastFullMonth + dDiff + (m1.date() - daysInLastFullMonth);
-            } else {
-                dDiff = daysInLastFullMonth + dDiff;
-            }
-            mDiff--;
-        }
-        if (mDiff < 0) {
-            mDiff = 12 + mDiff;
-            yDiff--;
-        }
+      var yDiff = m2.year() - m1.year();
+      var mDiff = m2.month() - m1.month();
+      var dDiff = m2.date() - m1.date();
+      var hourDiff = m2.hour() - m1.hour();
+      var minDiff = m2.minute() - m1.minute();
+      var secDiff = m2.second() - m1.second();
 
-        function pluralize(num, word) {
-            return num + ' ' + STRINGS[word + (num === 1 ? '' : 's')];
+      if (secDiff < 0) {
+        secDiff = 60 + secDiff;
+        minDiff--;
+      }
+      if (minDiff < 0) {
+        minDiff = 60 + minDiff;
+        hourDiff--;
+      }
+      if (hourDiff < 0) {
+        hourDiff = 24 + hourDiff;
+        dDiff--;
+      }
+      if (dDiff < 0) {
+        var daysInLastFullMonth = moment(m2.year() + '-' + (m2.month() + 1), "YYYY-MM")
+          .subtract(1, 'months').daysInMonth();
+        if (daysInLastFullMonth < m1.date()) { // 31/01 -> 2/03
+          dDiff = daysInLastFullMonth + dDiff + (m1.date() - daysInLastFullMonth);
+        } else {
+          dDiff = daysInLastFullMonth + dDiff;
         }
-        var result = [];
+        mDiff--;
+      }
+      if (mDiff < 0) {
+        mDiff = 12 + mDiff;
+        yDiff--;
+      }
 
-        if (yDiff) {
-            result.push(pluralize(yDiff, 'year'));
-        }
-        if (mDiff) {
-            result.push(pluralize(mDiff, 'month'));
-        }
-        if (dDiff) {
-            result.push(pluralize(dDiff, 'day'));
-        }
-        if (hourDiff) {
-            result.push(pluralize(hourDiff, 'hour'));
-        }
-        if (minDiff) {
-            result.push(pluralize(minDiff, 'minute'));
-        }
-        if (secDiff) {
-            result.push(pluralize(secDiff, 'second'));
-        }
+      var result = [];
 
-        return result.join(STRINGS.delimiter);
+      moment.relativeTimeThreshold('s',60);
+      moment.relativeTimeThreshold('m',60);
+      moment.relativeTimeThreshold('h',23);
+      moment.relativeTimeThreshold('dd',28);
+      moment.relativeTimeThreshold('dm',45);
+      moment.relativeTimeThreshold('dy',365);
+
+      function addUnit(value, unit) {
+        if( !opts.short ) return moment.duration(value,unit).humanize();
+        else return value+shortUnits[unit];
+      }
+
+      if (yDiff && opts.year) {
+        result.push( addUnit(yDiff,'year') );
+      }
+      if (mDiff && opts.month) {
+        result.push( addUnit( mDiff,'month') );
+      }
+      if (dDiff && opts.day) {
+        result.push( addUnit( dDiff,'day') );
+      }
+      if (hourDiff && opts.hour) {
+        result.push( addUnit( hourDiff,'hour') );
+      }
+      if (minDiff && opts.minute) {
+        result.push( addUnit( minDiff,'minute') );
+      }
+      if (secDiff && opts.second) {
+        result.push( addUnit( secDiff,'second') );
+      }
+
+      return result.join(' ');
     };
 }(moment));
